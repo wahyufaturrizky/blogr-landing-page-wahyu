34d4fd39b98b51d62bcf7c49bbbf80f821424997
WAHYU FATUR RIZKI <wahyufaturrizkyy@gmail.com>
2022-03-05 14:57:31 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      Product Overview Pricing Marketplace Features Integrations Company About
+      Team Blog Careers Connect Contact Newsletter LinkedIn Login Sign Up A
+      modern publishing platform Grow your audience and build your online brand
+      Start for Free Learn More Designed for the future Introducing an
+      extensible editor Blogr features an exceedingly intuitive interface which
+      lets you focus on one thing: creating content. The editor supports
+      management of multiple blogs and allows easy manipulation of embeds such
+      as images, videos, and Markdown. Extensibility with plugins and themes
+      provide easy ways to add functionality or change the looks of a blog.
+      Robust content management Flexible content management enables users to
+      easily move through posts. Increase the usability of your blog by adding
+      customized categories, sections, format, or flow. With this functionality,
+      you’re in full control. State of the Art Infrastructure With reliability
+      and speed in mind, worldwide data centers provide the backbone for
+      ultra-fast connectivity. This ensures your site will load instantly, no
+      matter where your readers are, keeping your site competitive. Free, open,
+      simple Blogr is a free and open source application backed by a large
+      community of helpful developers. It supports features such as code syntax
+      highlighting, RSS feeds, social media integration, third-party commenting
+      tools, and works seamlessly with Google Analytics. The architecture is
+      clean and is relatively easy to learn. Powerful tooling Batteries
+      included. We built a simple and straightforward CLI tool that makes
+      customization and deployment a breeze, but capable of producing even the
+      most complicated sites. Product Overview Pricing Marketplace Features
+      Integrations Company About Team Blog Careers Connect Contact Newsletter
+      LinkedIn
+    </main>
   );
 }
 
